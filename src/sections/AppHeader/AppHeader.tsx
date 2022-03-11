c0cb330b40cdc48cb7e1ebd171b0865c3f86eb27
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-03-11 18:26:16 +0300

    feat: add hamburger menu and theme-changer buttun with context state function

--- src/sections/AppHeader/AppHeader.tsx
+++ src/sections/AppHeader/AppHeader.tsx
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import GitHubDark from "./assets/GitHub-Mark-32px.png";
+import GitHubLight from "./assets/GitHub-Mark-Light-32px.png";
+import { Rotate as Hamburger } from "hamburger-react";
+import { ThemeContext } from "../../lib/components/Theme";
+import LightIcon from "./assets/brightness.png";
+import DarkIcon from "./assets/moon.png";
 
-export const AppHeader = () => {
-  const [menuState, setMenuState] = useState(false);
-  const currentMenuState = menuState ? "open" : "hidden";
-
-  const handleChange = (isChecked: boolean) => {
-    setMenuState(isChecked);
-  };
+export const AppHeader = ({ changeTheme }: { changeTheme: () => void }) => {
+  const [isOpen, setOpen] = useState(false);
+  const currentMenuState = isOpen ? "open" : "hidden";
+  const currentTheme = useContext(ThemeContext);
+  const themeChangerIcon = currentTheme === "light" ? LightIcon : DarkIcon;
+  const githubIcon = currentTheme === "light" ? GitHubDark : GitHubLight;
 
   return (
     <header className="header wrapper-xl row">
-      <input
-        type="checkbox"
-        name="menu-state-changer"
-        id="menu-state-changer"
-        checked={menuState}
-        onChange={(e) => handleChange(e.target.checked)}
+      <Hamburger
+        toggled={isOpen}
+        toggle={setOpen}
+        size={28}
+        color={"hsla(0, 0%, 10%, 1)"}
       />
-      <div className="header__logo col col-sm-12 col-md-3">
+      <div className="wrapper header__logo col col-sm-12 col-md-3">
         <p className="header__text">
           <span className="text_highlighted-red">Dev</span>challenges.io
         </p>
@@ -48,10 +51,20 @@ export const AppHeader = () => {
           </li>
         </ul>
         <div className="theme-changer">
-          <label>
-            <span className="theme-changer__text">Appearance</span>
-            <button className="theme-changer__button" type="button"></button>
-          </label>
+          <span className="theme-changer__text">Appearance</span>
+          <button
+            onClick={changeTheme}
+            className="theme-changer__button"
+            type="button"
+          >
+            <div className={`icon-wrapper icon-wrapper_${currentTheme}`}>
+              <img
+                className="theme-changer__icon"
+                src={themeChangerIcon}
+                alt="Theme Icon"
+              />
+            </div>
+          </button>
         </div>
         <div className="social">
           <a
@@ -59,7 +72,7 @@ export const AppHeader = () => {
             rel="noreferrer"
             target="_blank"
           >
-            <img src={GitHubDark} alt="Github Logo" />
+            <img src={githubIcon} alt="Github Logo" />
             <span className="visually-hidden">Github</span>
           </a>
         </div>
