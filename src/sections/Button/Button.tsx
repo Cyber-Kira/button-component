8078368dbfeaae05b9febc68d759de46299ef6cb
kirill <kirill@pop-os.localdomain>
2022-03-22 08:56:16 +0300

    feat: add button component

--- src/sections/Button/Button.tsx
+++ src/sections/Button/Button.tsx
@@ -1,5 +1,49 @@
 import React from "react";
 
-export const Button = () => {
-  return <button>Click Me</button>;
+enum variants {
+  outline = "btn_outline",
+  text = "btn_text",
+}
+
+enum colors {
+  default = "btn_default",
+  primary = "btn_primary",
+  secondary = "btn_secondary",
+  danger = "btn_danger",
+}
+
+enum sizes {
+  sm = "btn_sm",
+  md = "btn_md",
+  lg = "btn_lg",
+}
+
+interface Props {
+  text?: string;
+  variant?: variants;
+  color?: colors;
+  size?: sizes;
+  disabled?: boolean;
+  disableShadow?: boolean;
+}
+
+export const Button = ({
+  text = "Default",
+  variant,
+  color,
+  size,
+  disabled,
+  disableShadow,
+}: Props) => {
+  const styles = `
+  btn
+  ${variant ? variant : ""} 
+  ${color ? color : ""} 
+  ${size ? size : ""}
+  ${disabled ? "btn_disabled" : ""} 
+  ${disableShadow ? "btn_disable-shadow" : ""}`
+    .replace(/\s+/g, " ")
+    .trim();
+
+  return <button className={styles}>{text}</button>;
 };
