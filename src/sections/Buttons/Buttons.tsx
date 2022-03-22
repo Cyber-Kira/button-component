1d87d3b444677c3b1951b2d07cb305302d686f99
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-03-22 19:46:17 +0300

    wip: add various types of buttons

--- src/sections/Buttons/Buttons.tsx
+++ src/sections/Buttons/Buttons.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Button } from "../Button";
+import { colors, sizes, variants } from "../Button/types";
 import { CodeBox } from "../CodeBox";
 import { Typography } from "../Typography";
 import { mainButtonCodeSample } from "./data";
 
 export const Buttons = () => {
-  const { Item, Toggle } = CodeBox;
+  const { Item, Items, Toggle } = CodeBox;
   const { Header, Paragraph, Text } = Typography;
 
   return (
@@ -15,9 +16,20 @@ export const Buttons = () => {
       <Header level={2}>Examples</Header>
       <div className="content">
         <CodeBox>
-          <Item>
+          <Items>
+            <Button />
+            <Button variant={variants.outline} color={colors.secondary} />
+            <Button variant={variants.text} />
             <Button disableShadow />
-          </Item>
+            <Button disabled />
+            <Button variant={variants.text} disabled />
+          </Items>
+          <Items>
+            <Button />
+            <Button color={colors.primary} />
+            <Button color={colors.secondary} />
+            <Button color={colors.danger} />
+          </Items>
           <Item title="Type">
             There are <Text code>primary</Text> button,{" "}
             <Text code>default</Text> button, text button and{" "}
