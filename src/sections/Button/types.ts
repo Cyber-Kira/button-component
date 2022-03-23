79e096c44236c1c3700c8574fe37292d7e7afec0
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-03-23 11:22:38 +0300

    refactor: change enum to type

--- src/sections/Button/types.ts
+++ src/sections/Button/types.ts
@@ -1,20 +1,8 @@
-export enum variants {
-  outline = "btn_outline",
-  text = "btn_text",
-}
+export type variants = "outline" | "text";
 
-export enum colors {
-  default = "btn_default",
-  primary = "btn_primary-color",
-  secondary = "btn_secondary-color",
-  danger = "btn_danger-color",
-}
+export type colors = "defaul" | "primary" | "secondary" | "danger";
 
-export enum sizes {
-  sm = "btn_sm",
-  md = "btn_md",
-  lg = "btn_lg",
-}
+export type sizes = "sm" | "md" | "lg";
 
 export interface Props {
   text?: string;
