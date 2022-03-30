1b7ea554220e9ddbc2022446e27518c1cf582da6
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-03-30 22:24:25 +0300

    refactor: separate data

---
+++ src/data.ts
@@ -0,0 +1,27 @@
+import { DataItem } from "./sections/Menu/types";
+export const data: DataItem[] = [
+  {
+    title: "Colors",
+    link: "#",
+  },
+  {
+    title: "Typography",
+    link: "#",
+  },
+  {
+    title: "Spaces",
+    link: "#",
+  },
+  {
+    title: "Buttons",
+    link: "/buttons",
+  },
+  {
+    title: "Inputs",
+    link: "/inputs",
+  },
+  {
+    title: "Grid",
+    link: "#",
+  },
+];
