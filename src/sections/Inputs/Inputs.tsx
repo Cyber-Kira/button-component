a0de93632776808f65bccd64683bee64d729ce61
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-03-30 22:24:05 +0300

    feat: add more sections with inputs"

--- src/sections/Inputs/Inputs.tsx
+++ src/sections/Inputs/Inputs.tsx
@@ -3,8 +3,6 @@ import { CodeBox } from "../CodeBox";
 import { Input } from "../Input";
 import { Typography } from "../Typography";
 import KeyIcon from "@mui/icons-material/Key";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export const Inputs = () => {
   const { Item, Items, Toggle, CopyToClipboard } = CodeBox;
@@ -24,10 +22,158 @@ export const Inputs = () => {
             <Input
               placeholder="Basic usage"
               label="Label"
+              fullWidth
               helperText="LabelHelper"
             />
           </Items>
           <Item title="Basic usage">Basic usage example.</Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input placeholder="Input with fixed width" label="Label" />
+            <Input size="sm" startIcon={<KeyIcon />} />
+          </Items>
+          <Item title="Fixed width">Basic usage example.</Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input
+              placeholder="Input password"
+              label="Label"
+              fullWidth
+              error
+              helperText="LabelHelper"
+            />
+          </Items>
+          <Item title="Error">Error state of input.</Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input
+              placeholder="Medium size"
+              label="Label"
+              fullWidth
+              size="md"
+            />
+            <Input placeholder="Small size" label="Label" fullWidth size="sm" />
+          </Items>
+          <Item title="Sizes">
+            There are three sizes of an Input box: md (56px), sm (40px).
+          </Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input placeholder="" label="Label" fullWidth multiline row="2" />
+            <Input
+              placeholder="Max number of rows is 6"
+              label="Label"
+              fullWidth
+              multiline
+              row="6"
+            />
+          </Items>
+          <Item title="TextArea">For multi-line input.</Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input
+              placeholder="Input password"
+              label="Label"
+              fullWidth
+              password
+            />
+          </Items>
+          <Item title="Password">Input type of password.</Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input
+              placeholder="Input with start icon"
+              label="Label"
+              fullWidth
+              startIcon={<KeyIcon />}
+            />
+            <Input
+              placeholder="Input with end icon"
+              label="Label"
+              fullWidth
+              endIcon={<KeyIcon />}
+            />
+          </Items>
+          <Item title="Icons">Input type with icons.</Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input
+              placeholder="Input with value"
+              label="Label"
+              fullWidth
+              value="Some value"
+            />
+            <Input
+              placeholder="Input with value"
+              label="Label"
+              fullWidth
+              multiline
+              row="3"
+              value="Some value"
+            />
+          </Items>
+          <Item title="Value">
+            The <Text code>value</Text> attribute specifies the value of an{" "}
+            <Text code>Input</Text> element.
+          </Item>
+          <Item buttons>
+            <Toggle />
+            <CopyToClipboard textToCopy={"text"} />
+          </Item>
+          <Item code>{"text"}</Item>
+        </CodeBox>
+        <CodeBox>
+          <Items>
+            <Input
+              placeholder="Disabled input"
+              label="Label"
+              fullWidth
+              disabled
+            />
+          </Items>
+          <Item title="Disabled">Disabled input.</Item>
           <Item buttons>
             <Toggle />
             <CopyToClipboard textToCopy={"text"} />
---
+++ src/sections/Inputs/data.ts
@@ -0,0 +1,11 @@
+export const mainButtonCodeSample = `
+import { Button } from 'devch';
+
+ReactDOM.render(
+  <>
+    <Button />
+    <Button variant="outline" />
+    <Button variant="text" />
+  </>,
+  mountNode,
+);`;
