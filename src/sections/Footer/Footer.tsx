782d13b7dafca40d0c396d6eeefc870ce5a7a9eb
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-03-30 22:24:45 +0300

    feat: add small component to represent footer

---
+++ src/sections/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      created by
+      <a
+        href="https://devchallenges.io/portfolio/Cyber-Kira"
+        target="_blank"
+        rel="noreferrer"
+        className="footer_link"
+      >
+        CyberKira
+      </a>
+      - devChallenges.io
+    </div>
+  );
+};
