19994969d882412c0728312a630f2ec4e76e70fd
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-03-30 22:25:16 +0300

    feat: add menu with component on mobile view

---
+++ src/sections/Drawer/Drawer.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Squash as Hamburger } from "hamburger-react";
+import { data } from "../../data";
+import { ItemGroup } from "../Menu/components/ItemGroup";
+
+export const Drawer = () => {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <>
+      <div className="drawer-mask"></div>
+      <div className={`drawer ${isOpen ? "drawer-open" : ""}`}>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          size={18}
+          color={"hsla(0, 0%, 10%, 1)"}
+        />
+      </div>
+      <div className={`drawer-content ${isOpen ? "drawer-content-open" : ""}`}>
+        <ItemGroup header={"General"} items={data} />
+      </div>
+    </>
+  );
+};
